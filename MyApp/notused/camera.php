fe88c4498795880eaf189dade2201a116a34ef9d
AdamAufar <47731800+AdamAufar@users.noreply.github.com>
2024-06-14 13:41:50 +0700

    Delete MyApp directory

--- MyApp/notused/camera.php
+++
@@ -1,9 +0,0 @@
-<button id="startCamera">Take Picture</button>
-<video id="video" autoplay></video>
-<button id="capture" style="display: none;">Capture Photo</button>
-<canvas id="canvas" style="display: none;"></canvas>
-
-<form id="photoForm" action="upload.php" method="post" enctype="multipart/form-data" style="display: none;">
-    <input type="hidden" name="photo" id="photo">
-    <input type="submit" value="Upload Photo">
-</form>
